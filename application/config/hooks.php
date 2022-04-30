dc11643c8949f794deeabd8623367e41ee194ad8
Retno Sonjaya <beno.sons@khansia.co.id>
2022-04-30 11:51:34 +0700

    Commit message

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -1,6 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+$hook['pre_system'] = function() {
+    $dotenv = Dotenv\Dotenv::create(APPPATH);
+    $dotenv->load();
+   };
 /*
 | -------------------------------------------------------------------------
 | Hooks
